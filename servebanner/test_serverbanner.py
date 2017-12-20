8b9b9e0236cc4908cad5080bfa5b78733fe95557
Unknown <hhappak@gmail.com>
2017-12-20 12:11:04 +0100

    Reformat File
    Aethetic reformating of the file

--- servebanner/test_serverbanner.py
+++ servebanner/test_serverbanner.py
@@ -5,9 +5,11 @@ from bson import ObjectId
 from servebanner import servebanner
 import mongomock
 
+
 def test_valid_campaign_id():
     assert servebanner.valid_campaign_id(5) == True
     assert servebanner.valid_campaign_id("hi") == False
+
 
 def test_get_latest_data_collection():
     collection = mongomock.MongoClient().db.collection
@@ -15,7 +17,8 @@ def test_get_latest_data_collection():
     for obj in objects:
         obj['_id'] = collection.insert(obj)
     val = servebanner.get_latest_data_collection(collection)
-    assert(val == 2)
+    assert (val == 2)
+
 
 """
 def test_set_banner_counts():
@@ -30,8 +33,9 @@ def test_set_banner_counts():
     assert camp_stats.impressions_count == 2
 """
 
+
 def test_generate_unique_list_of_banners():
-    banner_ids = [6,5,4,3,2,1]
+    banner_ids = [6, 5, 4, 3, 2, 1]
     seen_banners = [1]
     result = servebanner.generate_unique_list_of_banners(banner_ids, seen_banners, 5)
 
@@ -42,12 +46,12 @@ def test_generate_unique_list_of_banners():
 
 def test_get_list_of_banners_revenue_with_revenue():
     collection = mongomock.MongoClient().db.collection
-    objects =  [{'impression_count':1, 'revenue_total':10, 'click_count':1, 'campaign_id':40, 'banner_id':1},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 2},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 3},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 4},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 5},
-                {'impression_count': 1, 'revenue_total': 0, 'click_count': 1, 'campaign_id': 40, 'banner_id': 6}]
+    objects = [{'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 1},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 2},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 3},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 4},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 5},
+               {'impression_count': 1, 'revenue_total': 0, 'click_count': 1, 'campaign_id': 40, 'banner_id': 6}]
     for obj in objects:
         obj['_id'] = collection.insert(obj)
     val = servebanner.get_list_of_banners_revenue(collection, 40, [], 5)
@@ -55,14 +59,15 @@ def test_get_list_of_banners_revenue_with_revenue():
     assert 6 not in val
     assert len(val) == 5
 
+
 def test_get_list_of_banners_revenue_pick_at_random():
     collection = mongomock.MongoClient().db.collection
-    objects =  [{'impression_count':1, 'revenue_total':10, 'click_count':1, 'campaign_id':40, 'banner_id':1},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 30, 'banner_id': 2},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 430, 'banner_id': 3},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 420, 'banner_id': 4},
-                {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 450, 'banner_id': 5},
-                {'impression_count': 1, 'revenue_total': 0, 'click_count': 1, 'campaign_id': 460, 'banner_id': 6}]
+    objects = [{'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 40, 'banner_id': 1},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 30, 'banner_id': 2},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 430, 'banner_id': 3},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 420, 'banner_id': 4},
+               {'impression_count': 1, 'revenue_total': 10, 'click_count': 1, 'campaign_id': 450, 'banner_id': 5},
+               {'impression_count': 1, 'revenue_total': 0, 'click_count': 1, 'campaign_id': 460, 'banner_id': 6}]
     for obj in objects:
         obj['_id'] = collection.insert(obj)
     val = servebanner.get_list_of_banners_revenue(collection, 40, [], 5)
@@ -70,8 +75,10 @@ def test_get_list_of_banners_revenue_pick_at_random():
     assert 1 in val
     assert len(val) == 5
 
+
 def test_get_seen_banners():
     val = servebanner.get_seen_banners("bla bla bla")
     assert val == []
-    val = servebanner.get_seen_banners("mycookiee=[373, 124, 114, 472, 322]; seen_banners_cookie=[446, 121, 334, 500, 182]")
+    val = servebanner.get_seen_banners(
+        "mycookiee=[373, 124, 114, 472, 322]; seen_banners_cookie=[446, 121, 334, 500, 182]")
     assert val == [446, 121, 334, 500, 182]
